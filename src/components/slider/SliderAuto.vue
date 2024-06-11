5136824389d0707edf564886196a853a8a9902b7
quangvm97 <quangvm@mor.com.vn>
2024-06-11 17:37:07 +0700

    [fe]update slider auto

--- src/components/slider/SliderAuto.vue
+++ src/components/slider/SliderAuto.vue
@@ -1,15 +1,77 @@
 <script setup>
-const props = defineProps(['dataSrc'])
+import { ref, computed, onMounted, onUnmounted, nextTick } from 'vue'
+
+const props = defineProps({
+  dataSrc: {
+    type: Array,
+    default: () => []
+  },
+  gap: {
+    type: Number,
+    default: 30
+  },
+  mode: {
+    type: String,
+    default: 'number',
+    validator: (value) => ['number', 'gap'].includes(value)
+  },
+  time: {
+    type: Number,
+    default: 10
+  },
+  direction: {
+    type: String,
+    default: 'left',
+    validator: (value) => ['left', 'right'].includes(value)
+  },
+  initPosition: {
+    type: String,
+    default: 'left',
+    validator: (value) => ['left', 'right', 'center'].includes(value)
+  }
+})
+
+const total = computed(() => props.dataSrc.length)
+const sliderRef = ref(null)
+const wrapItemRef = ref(null)
+const widthSlider = ref(0)
+const widthWrapItem = ref(0)
+
+const resize = () => {
+  widthSlider.value = sliderRef.value?.offsetWidth ?? 0
+  widthWrapItem.value = wrapItemRef.value?.offsetWidth ?? 0
+}
+
+const style = computed(() => {
+  return {
+    '--width-wrap': `${widthWrapItem.value}px`,
+    '--width-slider': `${widthSlider.value}px`,
+    '--time': `${props.time}s`,
+    '--first-wrap-delay': `0s`,
+    '--last-wrap-delay': `${props.time}s`,
+    '--gap': `${props.gap}px`
+  }
+})
+
+onMounted(async () => {
+  await nextTick()
+  console.log('wrapItemRef.value?.offsetWidth', wrapItemRef.value?.offsetWidth)
+  resize()
+  window.addEventListener('resize', resize)
+})
+onUnmounted(() => {
+  window.removeEventListener('resize', resize)
+})
 </script>
 
 <template>
-  <div class="slider">
-    <div class="slider-wrap">
+  <div class="slider" ref="sliderRef" :style="style">
+    <div class="slider-wrap" v-if="!!total" ref="wrapItemRef">
       <div class="slider-item" v-for="(item, i) in props.dataSrc" :key="i">
         <img :src="item.src" />
       </div>
     </div>
-    <div class="slider-wrap">
+    <div class="slider-wrap" v-if="!!total">
       <div class="slider-item" v-for="(item, i) in props.dataSrc" :key="i">
         <img :src="item.src" />
       </div>
@@ -20,13 +82,41 @@ const props = defineProps(['dataSrc'])
 <style scoped lang="scss">
 .slider {
   width: 100%;
-  display: inline-block;
+  height: 40px;
+  border: 1px solid red;
+  display: flex;
+  gap: var(--gap);
+  position: relative;
   .slider-wrap {
+    height: 40px;
+    position: absolute;
     display: flex;
-    gap: 10px;
+    gap: var(--gap);
+    left: 100%;
+    animation: slide var(--time) linear infinite;
+    &:first-child {
+      animation-delay: var(--first-wrap-delay);
+    }
+    &:last-child {
+      animation-delay: var(--last-wrap-delay);
+    }
+    .slider-item {
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      img {
+        width: auto;
+        height: 100%;
+      }
+    }
   }
-  .slider-item {
-    img {
+  @keyframes slide {
+    from {
+      left: 100%;
+    }
+    to {
+      left: var(--width-wrap);
     }
   }
 }
--- src/constant/index.js
+++ src/constant/index.js
@@ -1,13 +1,13 @@
 export const logoImage = {
-  img1: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Flogo&psig=AOvVaw0y0cH8VO7GUsOBHZGcxmLO&ust=1718098695152000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJjZ-8Xe0IYDFQAAAAAdAAAAABAE',
-  img2: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fvectors%2Flogo-design&psig=AOvVaw0y0cH8VO7GUsOBHZGcxmLO&ust=1718098695152000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJjZ-8Xe0IYDFQAAAAAdAAAAABAJ',
-  img3: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fazco.vn%2Flogo-la-gi%2F&psig=AOvVaw0y0cH8VO7GUsOBHZGcxmLO&ust=1718098695152000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJjZ-8Xe0IYDFQAAAAAdAAAAABAS',
-  img4: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fvn4u.vn%2Fbai-viet-gioi-thieu-ve-logo%2F&psig=AOvVaw0y0cH8VO7GUsOBHZGcxmLO&ust=1718098695152000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJjZ-8Xe0IYDFQAAAAAdAAAAABAc',
-  img5: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fsuno.vn%2Fblog%2F4-yeu-cho-mot-logo-hoan-hao%2F&psig=AOvVaw0y0cH8VO7GUsOBHZGcxmLO&ust=1718098695152000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJjZ-8Xe0IYDFQAAAAAdAAAAABAm',
-  img6: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Finhoangha.com%2Ftin-tuc%2Flogo-starbucks&psig=AOvVaw0y0cH8VO7GUsOBHZGcxmLO&ust=1718098695152000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJjZ-8Xe0IYDFQAAAAAdAAAAABBY',
-  img7: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Flogos-world.net%2Fhuawei-logo%2F&psig=AOvVaw0y0cH8VO7GUsOBHZGcxmLO&ust=1718098695152000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJjZ-8Xe0IYDFQAAAAAdAAAAABBi',
-  img8: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Flogogeek.uk%2Flogo-design%2Fwhy-logos-matter%2F&psig=AOvVaw0y0cH8VO7GUsOBHZGcxmLO&ust=1718098695152000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJjZ-8Xe0IYDFQAAAAAdAAAAABB2',
-  img9: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fblog.hubspot.com%2Fmarketing%2Fgoogle-logo-history&psig=AOvVaw0y0cH8VO7GUsOBHZGcxmLO&ust=1718098695152000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqGAoTCJjZ-8Xe0IYDFQAAAAAdAAAAABCAAQ',
+  img1: 'https://img.freepik.com/free-vector/bird-colorful-gradient-design-vector_343694-2506.jpg',
+  img2: 'https://png.pngtree.com/png-clipart/20201208/original/pngtree-red-and-black-logo-png-image_5517319.jpg',
+  img3: 'https://upload.wikimedia.org/wikipedia/commons/e/e8/Tesla_logo.png',
+  img4: 'https://upload.wikimedia.org/wikipedia/commons/8/88/Puma-Logo.png',
+  img5: 'https://seeklogo.com/images/V/viet-nam-logo-2242749340-seeklogo.com.png',
+  img6: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKCAHCxWwutWINsktvMA8P3fCCPOKZuHQgWQ&s',
+  img7: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdhjAi70Ivmsm3azJ7VPrC_CZFcqM8T4seG-R75hQv6HXdpEeyCoIW0lunR0nNJHOrNUg&usqp=CAU',
+  img8: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa_TkP9RCQyYMqChKD0q6Nszpz1ENBmWc_yQ&s',
+  img9: 'https://upload.wikimedia.org/wikipedia/commons/2/24/Adidas_logo.png',
   img10:
-    'https://www.google.com/url?sa=i&url=https%3A%2F%2Fdongphucsongphu.com%2Flogo%2Flogo-coca-cola.html&psig=AOvVaw0y0cH8VO7GUsOBHZGcxmLO&ust=1718098695152000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqGAoTCJjZ-8Xe0IYDFQAAAAAdAAAAABCKAQ'
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0k_KnGE71BCOy25YXpAbXYdXHIMpQIu3TXA&s'
 }
