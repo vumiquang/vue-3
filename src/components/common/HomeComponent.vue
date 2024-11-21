9fc803860f9a102005e3da0ff9b4c6b216ac2f48
quangvm97 <quangvm@mor.com.vn>
2024-11-21 10:54:51 +0700

    feat: multiselect

---
+++ src/components/common/HomeComponent.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref, reactive, computed } from 'vue'
+import Multiselect from '@vueform/multiselect'
+import '@vueform/multiselect/themes/default.css'
+const props = defineProps(['valueProps'])
+const emit = defineEmits(['update'])
+
+const valueA = computed({
+  get() {
+    return props.valueProps
+  },
+  set(value) {
+    // console.log('==>'.value)
+    // emit('update', value)
+  }
+})
+
+const options = ['a', 'b', 'c']
+</script>
+
+<template>
+  <div>
+    <Multiselect v-model="valueA.id" :options="options" class="multiselect-blue" />
+  </div>
+</template>
+
+<style scoped></style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,7 +1,23 @@
-<script setup></script>
+<script setup>
+import { ref, reactive, computed } from 'vue'
+
+import HomeComponent from '../components/common/HomeComponent.vue'
+
+const actionPlan = reactive({ id: 'a' })
+
+const update = (val) => {
+  console.log('-->', val)
+}
+</script>
 
 <template>
-  <div>home page</div>
+  <div>
+    home page
+    <div>
+      aaa
+      <HomeComponent :valueProps="actionPlan" @update="update" />
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
